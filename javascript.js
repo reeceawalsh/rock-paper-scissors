036db22b24f2f12b5ba393c844789116b56f3647
reeceawalsh <reeceawalsh@hotmail.co.uk>
2021-07-30 23:57:53 +0100

    Clicking an option now changes the image in the larger image for both computer and player. It also updates the score above the larger image.
    Still need to fix an issue, the image doesn't reset back to the shaking fist animation after playing 1 round.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,137 @@
+// Variables
+const imgs = document.querySelectorAll(".option");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const playerRock = document.getElementById("playerRock");
+const playerScissors = document.getElementById("playerScissors");
+const playerPaper = document.getElementById("playerPaper");
+const computerRock = document.getElementById("computerRock");
+const computerScissors = document.getElementById("computerScissors");
+const computerPaper = document.getElementById("computerPaper");
+const playerScore = document.getElementById("playerScore");
+const computerScore = document.getElementById("computerScore");
+
 // Keep track of which player won a round
 let playerWinCount = 0;
 let computerWinCount = 0;
+let playerSelection = "";
 
 // Computers choice of Rock, Paper or Scissors
 const computerPlay = () => {
   let choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * choices.length - 1) + 1];
 };
+
+// Determine player selection
+playerRock.addEventListener("click", () => {
+  playerSelection = "rock";
+  rock.classList.add("shake");
+  rock.addEventListener("animationend", removeClass);
+  game();
+});
+
+playerPaper.addEventListener("click", () => {
+  playerSelection = "paper";
+  rock.classList.add("shake");
+  rock.addEventListener("animationend", removeClass);
+  game();
+});
+
+playerScissors.addEventListener("click", () => {
+  playerSelection = "scissors";
+  rock.classList.add("shake");
+  rock.addEventListener("animationend", removeClass);
+  game();
+});
+
+function removeClass() {
+  rock.classList.remove("shake");
+}
+
+function resetImages() {
+  rock.classList.add("hide");
+  paper.classList.add("hide");
+  scissors.classList.add("hide");
+  rock.classList.remove("show");
+  paper.classList.remove("show");
+  scissors.classList.remove("show");
+}
+
+function resetGame() {
+  rock.classList.remove("hide");
+  rock.classList.add("show");
+  paper.classList.remove("show");
+  paper.classList.add("hide");
+  scissors.classList.remove("show");
+  scissors.classList.add("hide");
+}
+
+function resetImages2() {
+  rock2.classList.add("hide");
+  paper2.classList.add("hide");
+  scissors2.classList.add("hide");
+  rock2.classList.remove("show");
+  paper2.classList.remove("show");
+  scissors2.classList.remove("show");
+}
+
+function resetGame() {
+  rock2.classList.remove("hide");
+  rock2.classList.add("show");
+  paper2.classList.remove("show");
+  paper2.classList.add("hide");
+  scissors2.classList.remove("show");
+  scissors2.classList.add("hide");
+}
+
+function changePlayerImage() {
+  if (playerSelection == "rock") {
+    resetImages();
+    rock.classList.remove("hide");
+    rock.classList.add("show");
+  }
+  if (playerSelection == "paper") {
+    resetImages();
+    paper.classList.remove("hide");
+    paper.classList.add("show");
+  }
+  if (playerSelection == "scissors") {
+    resetImages();
+    scissors.classList.remove("hide");
+    scissors.classList.add("show");
+  }
+}
+
+function changeComputerImage() {
+  if (computerSelection == "rock") {
+    resetImages2();
+    rock2.classList.remove("hide");
+    rock2.classList.add("show");
+  }
+  if (computerSelection == "paper") {
+    resetImages2();
+    paper2.classList.remove("hide");
+    paper2.classList.add("show");
+  }
+  if (computerSelection == "scissors") {
+    resetImages2();
+    scissors2.classList.remove("hide");
+    scissors2.classList.add("show");
+  }
+}
+
+function keepScore() {
+  playerScore.textContent = "Player: " + playerWinCount;
+  computerScore.textContent = "Computer: " + computerWinCount;
+}
+// imgs.forEach((img) => {
+//   img.addEventListener("click", () => {
+//     rock.classList.add("shake");
+//     rock.addEventListener("animationend", removeClass);
+//     rock.addEventListener("animationend", roll);
+//   });
+// });
 
 // List of outcomes from all the possible choices by computer and player
 const playRound = (playerSelection, computerSelection) => {
@@ -39,42 +164,35 @@ const playRound = (playerSelection, computerSelection) => {
   }
 };
 
-// Gameplay
+// game() loops through 'gamelength' amount of times and logs the score and who won that round each time
 const game = () => {
-  for (let i = 0; i < 5; i++) {
-    playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-    computerSelection = computerPlay().toLowerCase();
-    if (
-      playRound(playerSelection, computerSelection).startsWith("You Win!") ==
-      true
-    ) {
-      playerWinCount++;
-      console.log("You Win This Round");
-      console.log(
-        "Player: " + playerWinCount + " " + "Computer: " + computerWinCount
-      );
-    }
-    if (
-      playRound(playerSelection, computerSelection).startsWith("You Lose!") ==
-      true
-    ) {
-      computerWinCount++;
-      console.log("You Lost This Round");
-      console.log(
-        "Player: " + playerWinCount + " " + "Computer: " + computerWinCount
-      );
-    }
-    if (
-      playRound(playerSelection, computerSelection).startsWith("You Draw") ==
-      true
-    ) {
-      i--;
-      console.log("You Drew This Round");
-      console.log(
-        "Player: " + playerWinCount + " " + "Computer: " + computerWinCount
-      );
-    }
+  computerSelection = computerPlay().toLowerCase();
+  changePlayerImage();
+  changeComputerImage();
+  if (
+    playRound(playerSelection, computerSelection).startsWith("You Win!") == true
+  ) {
+    playerWinCount++;
+    console.log("You Win This Round");
+    keepScore();
   }
+  if (
+    playRound(playerSelection, computerSelection).startsWith("You Lose!") ==
+    true
+  ) {
+    computerWinCount++;
+    console.log("You Lost This Round");
+    keepScore();
+  }
+  // In order to make the game the best of 5 rounds, the function uses 'i--' here to discount draw rounds.
+  if (
+    playRound(playerSelection, computerSelection).startsWith("You Draw") == true
+  ) {
+    console.log("You Drew This Round");
+    keepScore();
+  }
+
+  // After the loop has finished it will display results based on which win count is higher
   if (playerWinCount > computerWinCount) {
     console.log("You Win This Game! Congratulations.");
   }
@@ -83,4 +201,32 @@ const game = () => {
   }
 };
 
-console.log(game());
+// Animation to shake the rock
+
+// imgs.forEach((img) => {
+//   img.addEventListener("click", () => {
+//     rock.classList.add("shake");
+//     rock.addEventListener("animationend", removeClass);
+//     rock.addEventListener("animationend", roll);
+//   });
+// });
+
+// Click an option and then rock shakes before being replaced by the option that was clicked
+
+function roll() {
+  rock.addEventListener("mouseoff", rockRoll);
+  paper.addEventListener("mouseoff", paperRoll);
+  scissors.addEventListener("mouseoff", scissorsRoll);
+}
+
+function rockRoll() {
+  rock.classList.remove("hide");
+}
+
+function scissorsRoll() {
+  scissors.classList.remove("hide");
+}
+
+function paperRoll() {
+  paper.classList.remove("hide");
+}
