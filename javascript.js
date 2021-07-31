65dbd9f8f49b7a6cbdbecdd3b4c4f51d35f25c9c
reeceawalsh <reeceawalsh@hotmail.co.uk>
2021-07-31 15:59:46 +0100

    Completely refactored all of the js code and made it a lot cleaner. Also added comments for better understandability later on
    I will be re-styling and adding some media queries next

--- javascript.js
+++ javascript.js
@@ -1,242 +1,132 @@
-// Variables
-const imgs = document.querySelectorAll(".option");
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
-const rock2 = document.getElementById("rock2");
-const paper2 = document.getElementById("paper2");
-const scissors2 = document.getElementById("scissors2");
+// These variables are three pictures which will be displayed in the large box above the three smaller images. I need these variables so that I can show the option the player or computer picks and hide the other options.
+const largeImages = document.getElementsByClassName("largeImg");
+const bothRocks = document.getElementsByClassName("rock");
+const bothPapers = document.getElementsByClassName("paper");
+const bothScissors = document.getElementsByClassName("scissors");
+
+// These variables are the three smaller images under the large image. They will be clickable, the option chosen will be displayed in the large box above them.
 const playerRock = document.getElementById("playerRock");
 const playerScissors = document.getElementById("playerScissors");
 const playerPaper = document.getElementById("playerPaper");
-const computerRock = document.getElementById("computerRock");
-const computerScissors = document.getElementById("computerScissors");
-const computerPaper = document.getElementById("computerPaper");
+
+// These variables will be used to update and display the score to the player.
 const playerScore = document.getElementById("playerScore");
 const computerScore = document.getElementById("computerScore");
+const roundComment = document.getElementById("roundComment");
 
-// Keep track of which player won a round
+// Keep track of the player's and computer's score.
 let playerWinCount = 0;
 let computerWinCount = 0;
-let playerSelection = "";
+let roundResult = "";
 
-// Computers choice of Rock, Paper or Scissors
+// Computers random choice of Rock, Paper or Scissors.
 const computerPlay = () => {
-  let choices = ["Rock", "Paper", "Scissors"];
+  let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length - 1) + 1];
 };
 
-// Determine player selection
+// Determine player selection by which of the smaller images the player selected.
 playerRock.addEventListener("click", () => {
   playerSelection = "rock";
-  rock.classList.add("shake");
-  rock.addEventListener("animationend", removeClass);
-  rock.addEventListener("animationend", game);
+  shake();
 });
-
 playerPaper.addEventListener("click", () => {
   playerSelection = "paper";
-  rock.classList.add("shake");
-  rock.addEventListener("animationend", removeClass);
-  rock.addEventListener("animationend", game);
+  shake();
 });
-
 playerScissors.addEventListener("click", () => {
   playerSelection = "scissors";
-  rock.classList.add("shake");
-  rock.addEventListener("animationend", removeClass);
-  rock.addEventListener("animationend", game);
+  shake();
 });
 
-function removeClass() {
-  rock.classList.remove("shake");
-  rock.classList.remove("show");
-  rock.classList.add("hide");
-  console.log("hiding");
-}
-
-function resetImages() {
-  rock.classList.add("hide");
-  paper.classList.add("hide");
-  scissors.classList.add("hide");
-  rock.classList.remove("show");
-  paper.classList.remove("show");
-  scissors.classList.remove("show");
-}
-
-function resetGame() {
-  rock.classList.remove("hide");
-  rock.classList.add("show");
-  paper.classList.remove("show");
-  paper.classList.add("hide");
-  scissors.classList.remove("show");
-  scissors.classList.add("hide");
-  rock2.classList.remove("hide");
-  rock2.classList.add("show");
-  paper2.classList.remove("show");
-  paper2.classList.add("hide");
-  scissors2.classList.remove("show");
-  scissors2.classList.add("hide");
-}
-
-function resetImages2() {
-  rock2.classList.add("hide");
-  paper2.classList.add("hide");
-  scissors2.classList.add("hide");
-  rock2.classList.remove("show");
-  paper2.classList.remove("show");
-  scissors2.classList.remove("show");
-}
-
-function changePlayerImage() {
-  if (playerSelection == "rock") {
-    resetImages();
-    rock.classList.remove("hide");
-    rock.classList.add("show");
+// The shake function will make the large rock image shake by adding a class that has an animation. This is to simulate the start of a rock, paper, scissors game. After the animation has ended, it will remove the shake class from both the players and the computers large rock. After the loop has ended, and the animation has ended for both rocks, the game function will begin.
+const shake = () => {
+  for (let i = 0; i < bothRocks.length; i++) {
+    bothRocks[i].classList.add("shake");
+    bothRocks[i].addEventListener("animationend", removeClass);
   }
-  if (playerSelection == "paper") {
-    resetImages();
-    paper.classList.remove("hide");
-    paper.classList.add("show");
-  }
-  if (playerSelection == "scissors") {
-    resetImages();
-    scissors.classList.remove("hide");
-    scissors.classList.add("show");
-  }
-}
+  bothRocks[0].addEventListener("animationend", game);
+};
 
-function changeComputerImage() {
-  if (computerSelection == "rock") {
-    resetImages2();
-    rock2.classList.remove("hide");
-    rock2.classList.add("show");
+// Just removes the shake class, ending the animation.
+const removeClass = () => {
+  for (let i = 0; i < bothRocks.length; i++) {
+    bothRocks[i].classList.remove("shake");
   }
-  if (computerSelection == "paper") {
-    resetImages2();
-    paper2.classList.remove("hide");
-    paper2.classList.add("show");
-  }
-  if (computerSelection == "scissors") {
-    resetImages2();
-    scissors2.classList.remove("hide");
-    scissors2.classList.add("show");
-  }
-}
+};
 
-function keepScore() {
+// Runs the reset images function in order to hide all of the images, then it removes the hide class from the images the player and the computer chose.
+const changeImages = () => {
+  resetImages();
+  document.getElementById(playerSelection).classList.remove("hide");
+  document.getElementById(computerSelection + "2").classList.remove("hide");
+};
+
+// Hides all of the images, ready for the chosen image to be displayed.
+const resetImages = () => {
+  for (let i = 0; i < largeImages.length; i++) {
+    largeImages[i].classList.add("hide");
+  }
+};
+
+// Runs the reset image function in order to hide all of the images, then removes the hide class from both rocks in order to display them. This is the display that we want before we start the game again.
+const resetGame = () => {
+  resetImages();
+  for (let i = 0; i < bothRocks.length; i++) {
+    bothRocks[i].classList.remove("hide");
+  }
+};
+
+// Updates the player's and computer's score.
+const keepScore = () => {
   playerScore.textContent = "Player: " + playerWinCount;
   computerScore.textContent = "Computer: " + computerWinCount;
-}
-// imgs.forEach((img) => {
-//   img.addEventListener("click", () => {
-//     rock.classList.add("shake");
-//     rock.addEventListener("animationend", removeClass);
-//     rock.addEventListener("animationend", roll);
-//   });
-// });
+};
 
-// List of outcomes from all the possible choices by computer and player
-const playRound = (playerSelection, computerSelection) => {
+// List of outcomes from all the possible choices by computer and player. The function checks who won, updates the comment to inform the user and updates the win count so that the score can be updated.
+const playRound = () => {
   if (playerSelection == "rock" && computerSelection == "paper") {
-    return "You Lose! Paper beats Rock";
+    roundComment.textContent = "You Lose! Paper beats Rock";
+    computerWinCount++;
   }
   if (playerSelection == "rock" && computerSelection == "rock") {
-    return "You Draw! Try Again.";
+    roundComment.textContent = "You Draw! Try Again.";
   }
   if (playerSelection == "rock" && computerSelection == "scissors") {
-    return "You Win! Rock beats Scissors";
+    roundComment.textContent = "You Win! Rock beats Scissors";
+    playerWinCount++;
   }
   if (playerSelection == "paper" && computerSelection == "rock") {
-    return "You Win! Paper beats Rock";
+    roundComment.textContent = "You Win! Paper beats Rock";
+    playerWinCount++;
   }
   if (playerSelection == "paper" && computerSelection == "paper") {
-    return "You Draw! Try again.";
+    roundComment.textContent = "You Draw! Try again.";
   }
   if (playerSelection == "paper" && computerSelection == "scissors") {
-    return "You Lose! Scissors beats Paper.";
+    roundComment.textContent = "You Lose! Scissors beats Paper.";
+    computerWinCount++;
   }
   if (playerSelection == "scissors" && computerSelection == "paper") {
-    return "You Win! Scissors beats Paper.";
+    roundComment.textContent = "You Win! Scissors beats Paper.";
+    playerWinCount++;
   }
   if (playerSelection == "scissors" && computerSelection == "scissors") {
-    return "You Draw! Try again.";
+    roundComment.textContent = "You Draw! Try again.";
   }
   if (playerSelection == "scissors" && computerSelection == "rock") {
-    return "You Lose! Rock beats Scissors.";
+    roundComment.textContent = "You Lose! Rock beats Scissors.";
+    computerWinCount++;
   }
 };
 
-// game() loops through 'gamelength' amount of times and logs the score and who won that round each time
+// Identifies computer selection, changes the images to the images the player and computer chose, plays a round to work out who won, updates the score and then resets the game a second after.
 const game = () => {
   computerSelection = computerPlay().toLowerCase();
-  changePlayerImage();
-  changeComputerImage();
+  changeImages();
+  playRound();
+  keepScore();
   setTimeout(resetGame, 1000);
-  if (
-    playRound(playerSelection, computerSelection).startsWith("You Win!") == true
-  ) {
-    playerWinCount++;
-    console.log("You Win This Round");
-    keepScore();
-  }
-  if (
-    playRound(playerSelection, computerSelection).startsWith("You Lose!") ==
-    true
-  ) {
-    computerWinCount++;
-    console.log("You Lost This Round");
-    keepScore();
-  }
-  // In order to make the game the best of 5 rounds, the function uses 'i--' here to discount draw rounds.
-  if (
-    playRound(playerSelection, computerSelection).startsWith("You Draw") == true
-  ) {
-    console.log("You Drew This Round");
-    keepScore();
-  }
-
-  // After the loop has finished it will display results based on which win count is higher
-  if (
-    playerWinCount + computerWinCount == 5 &&
-    playerWinCount > computerWinCount
-  ) {
-    console.log("You Win This Game! Congratulations.");
-  }
-  if (
-    playerWinCount + computerWinCount == 5 &&
-    computerWinCount > playerWinCount
-  ) {
-    console.log("You Lose This Game! Unlucky.");
-  }
 };
 
-// Animation to shake the rock
-
-// imgs.forEach((img) => {
-//   img.addEventListener("click", () => {
-//     rock.classList.add("shake");
-//     rock.addEventListener("animationend", removeClass);
-//     rock.addEventListener("animationend", roll);
-//   });
-// });
-
-// Click an option and then rock shakes before being replaced by the option that was clicked
-
-function roll() {
-  rock.addEventListener("mouseoff", rockRoll);
-  paper.addEventListener("mouseoff", paperRoll);
-  scissors.addEventListener("mouseoff", scissorsRoll);
-}
-
-function rockRoll() {
-  rock.classList.remove("hide");
-}
-
-function scissorsRoll() {
-  scissors.classList.remove("hide");
-}
-
-function paperRoll() {
-  paper.classList.remove("hide");
-}
+// There is currently no round limit set up or special message displayed when a side gets to a certain point.
